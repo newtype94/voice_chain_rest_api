e5082ec18ba1d6960de900f09d1a3b5843519b91
김용훈 <newtype94@korea.ac.kr>
2020-03-30 15:56:19 +0900

    connect lambdas with API gateway

--- index.js
+++ index.js
@@ -7,6 +7,33 @@ const ddb = new AWS.DynamoDB.DocumentClient({
   region: process.env.AWS_REGION
 });
 
+exports.hello = (event, context, callback) => {
+  switch (event.httpMethod) {
+    case "DELETE":
+      sendResponse(200, "DELETE happened", callback);
+      break;
+    case "GET":
+      sendResponse(200, "GET happened", callback);
+      break;
+    case "POST":
+      sendResponse(200, "POST happened", callback);
+      break;
+    case "PUT":
+      sendResponse(200, "PUT happened", callback);
+      break;
+    default:
+      sendResponse(200, `Unsupported method "${event.httpMethod}"`, callback);
+  }
+};
+
+function sendResponse(statusCode, message, callback) {
+  const response = {
+    statusCode: statusCode,
+    body: JSON.stringify(message)
+  };
+  callback(null, response);
+}
+
 exports.handler = (event, context, callback) => {
   const done = (err, res) =>
     callback(null, {
