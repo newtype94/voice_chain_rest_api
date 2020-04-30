a788a66e0f752ac6d7e665cbd73762649b2b6408
김용훈 <newtype94@korea.ac.kr>
2020-04-30 15:51:31 +0900

    Add authorizing block, data

--- src/block.js
+++ src/block.js
@@ -17,40 +17,75 @@ const ddb = new AWS.DynamoDB.DocumentClient({
 });
 
 exports.handler = async (event) => {
-  let scans;
-  const { index } = event.queryStringParameters;
-
-  const params = {
-    TableName,
-    ProjectionExpression: "*",
-  };
-
-  if (index > 0) {
-    params.FilterExpression = "#idx = between :start and :end";
-    params.ExpressionAttributeNames = {
-      "#idx": "index",
-    };
-    params.ExpressionAttributeValues = {
-      ":start": index,
-      ":end": index,
-    };
-  }
-
   if (event.httpMethod === "GET") {
+    let params = {
+      TableName,
+      ProjectionExpression: "#idx, #hs, previousHash, createdAt, #dt",
+      ExpressionAttributeNames: {
+        "#idx": "index",
+        "#hs": "hash",
+        "#dt": "data",
+      },
+    };
+
+    if (event.queryStringParameters) {
+      const { index, start, end } = event.queryStringParameters;
+      if (index) {
+        params.FilterExpression = "#idx = :index";
+        params.ExpressionAttributeValues = {
+          ":index": parseInt(index),
+        };
+      } else if (start && end) {
+        params.FilterExpression = "#idx between :start and :end";
+        params.ExpressionAttributeValues = {
+          ":start": parseInt(start),
+          ":end": parseInt(end),
+        };
+      }
+    }
+
+    let scans;
+
     try {
       scans = await ddb.scan(params).promise();
     } catch (err) {
       return {
         statusCode: err.statusCode || 501,
         headers,
-        body: "Failed to connect: " + JSON.stringify(err),
+        body: "Failed to connect" + JSON.stringify(err),
       };
     }
+
     return {
       statusCode: 200,
       headers,
-      body: JSON.stringify(scans) + JSON.stringify(event),
+      body: JSON.stringify(scans),
     };
+    //GET case end
+    //------------
+    //POST case start
   } else if (event.httpMethod === "POST") {
+    const input = JSON.parse(event.body);
+
+    try {
+      puts = await ddb
+        .put({
+          TableName,
+          Item: input,
+        })
+        .promise();
+    } catch (err) {
+      return {
+        statusCode: err.statusCode || 501,
+        headers,
+        body: "Failed to put.." + JSON.stringify(err) + JSON.stringify(input),
+      };
+    }
+
+    return {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify(puts),
+    };
   }
 };
--- src/check.js
+++ src/check.js
@@ -11,49 +11,85 @@ const headers = {
   "Access-Control-Allow-Methods": "*",
 };
 
-exports.handler = async (event) => {
-  const ddb = new AWS.DynamoDB.DocumentClient({
-    apiVersion: "2012-08-10",
-    region: process.env.AWS_REGION,
-  });
+const ddb = new AWS.DynamoDB.DocumentClient({
+  apiVersion: "2012-08-10",
+  region: process.env.AWS_REGION,
+});
 
-  const input = JSON.parse(event.body);
-  /*
-  if (
-    typeof input.userId !== number ||
-    typeof input.voiceHash !== string ||
-    typeof input.timeStamp !== number
-  )
+exports.handler = async (event) => {
+  if (!event.queryStringParameters)
     return {
       statusCode: 400,
       headers,
-      body: "Invalid type of input..",
+      body: "insert parameter on url..",
     };
-*/
-  const createdAt = new Date().getTime();
 
-  let puts;
-  let scans;
+  let {
+    index,
+    userId,
+    voiceHash,
+    timeStamp,
+    hash,
+    previousHash,
+    createdAt,
+  } = event.queryStringParameters;
+
+  if (!index || !userId || !voiceHash || !timeStamp)
+    return {
+      statusCode: 400,
+      headers,
+      body: "check your parameters on url..",
+    };
+
+  let gets, getsNext;
 
   try {
-    puts = await ddb
-      .put({
-        TableName,
-        Item: input,
-      })
+    index = parseInt(index);
+    gets = await ddb.get({ TableName, Key: { index } }).promise();
+    getsNext = await ddb
+      .get({ TableName, Key: { index: index + 1 } })
       .promise();
-    scans = await ddb.scan({ TableName }).promise();
   } catch (err) {
     return {
       statusCode: err.statusCode || 501,
       headers,
-      body: "Failed to connect: " + JSON.stringify(err),
+      body: "error occured when Get from Dynamodb.." + JSON.stringify(err),
     };
+  }
+
+  if (authorize.length !== 1)
+    return {
+      statusCode: 400,
+      headers,
+      body: "No match with index..",
+    };
+
+  let body;
+
+  if (hash && previousHash && createdAt) {
+    if (
+      gets.hash === hash &&
+      gets.previousHash === previousHash &&
+      gets.createdAt === parseInt(createdAt) &&
+      gets.data.userId === userId &&
+      gets.data.voiceHash === voiceHash &&
+      gets.data.timeStamp === parseInt(timeStamp)
+    )
+      body = "block success";
+    else body = "block fail";
+  } else {
+    if (
+      gets.data.userId === userId &&
+      gets.data.voiceHash === voiceHash &&
+      gets.data.timeStamp === parseInt(timeStamp)
+    )
+      body = "data success";
+    else body = "data fail";
   }
 
   return {
     statusCode: 200,
     headers,
-    body: JSON.stringify(puts) + "\n\n" + JSON.stringify(scans),
+    body,
   };
 };
